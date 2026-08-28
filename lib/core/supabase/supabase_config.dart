39dd69784d8dea641f075d99bf15b0cc6d7fa45a
jerrytei207 <jerrytei207@gmail.com>
2026-08-29 02:28:27 +0800

    changed supabase from publishable to secret key

--- lib/core/supabase/supabase_config.dart
+++ lib/core/supabase/supabase_config.dart
@@ -1,4 +1,4 @@
 class SupabaseConfig {
   static const String url = 'https://ahyqcqjopkvmgcpfguzr.supabase.co';
-  static const String publishableKey = 'sb_publishable_ggFOn6Jor0eUag4Knw3Odg_A2G79xk9';
+  static const String secretKey = 'sb_secret_ClQBQcUig-nDTRmDv4VLWg_jQ3qbcAj';
 }

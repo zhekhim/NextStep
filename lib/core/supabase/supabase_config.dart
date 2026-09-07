243cf97b9fa7e3fdb899b793c60236ac7150c263
jerrytei207 <jerrytei207@gmail.com>
2026-09-07 21:58:43 +0800

    secret key

--- lib/core/supabase/supabase_config.dart
+++ lib/core/supabase/supabase_config.dart
@@ -1,4 +1,4 @@
 class SupabaseConfig {
   static const String url = 'https://ahyqcqjopkvmgcpfguzr.supabase.co';
-  static const String secretKey = 'sb_secret_I2W5aegBrnG9ZKS8FhB1eA_FgzGig0B';
+  static const String secretKey = 'sb_secret_d0C0nlmOP7TdVHaVrB2IBA_B_US5dyt';
 }
